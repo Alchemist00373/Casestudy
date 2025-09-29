8ef954de269befad69cfb12f983407987fbaf100
hanie fe <haniejunio19@gmail.com>
2025-09-29 19:42:34 +0800

    added my portfolio

---
+++ src/portfolio/hanie/js/hanie.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the print button element
+    const printButton = document.getElementById('printButton');
+
+    if (printButton) {
+        printButton.addEventListener('click', () => {
+            window.print();
+        });
+    } else {
+        console.error("Print button with ID 'printButton' not found.");
+    }
+
+
+});
